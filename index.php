70de507016bd5b292ee8e0f5846d03b25f9ee941
juan david marin <juaanmarin2007@gmail.com>
2023-11-10 21:50:00 -0500

    se creo el esquema del proyecto y esqueleto de el formulario

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="resources/css/style.css">
+    <title>Send Mail</title>
+</head>
+<body>
+    <form class='form' action="" method="post">
+        <div class='left'>
+            <img src="resources/img/silky-sending-messages.png" alt="">
+            <!-- <img src="resources/img/florid-mailing-list-1.gif" alt=""> -->
+        </div>
+        <div class='right'>
+            <h2>Form</h2>
+            <div class="login__field-group">
+					<label class="login__label" for="user-email">Name</label>
+					<input class="login__field" id="user-email" type="text" name="name" />
+			</div>
+            <div class="login__field-group">
+					<label class="login__label" for="user-email">Email</label>
+					<input class="login__field" id="user-email" type="email" name="email" />
+			</div>
+            <div class="login__field-group">
+					<label class="login__label" for="user-email">Text</label>
+					<input class="login__field" id="user-email" type="text" name="text" />
+			</div>
+
+            <button >Send</button>
+        </div>
+        
+    </form>
+</body>
+</html>
